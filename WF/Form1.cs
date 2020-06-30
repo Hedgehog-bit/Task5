8e36cd58dd428c17258a2839621007618a1c3a42
Hedgehog-bit <65031811+Hedgehog-bit@users.noreply.github.com>
2020-06-30 14:41:19 +0300

    Add files via upload

---
+++ WF/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(textBox1.Text);
+                double b = double.Parse(textBox2.Text);
+                double h = double.Parse(textBox3.Text);
+                for (double i = a; i <= b; i += h)
+                    try
+                    {
+                        textBox4.Text += "y(" + i.ToString() + ")= " + F(i).ToString() + Environment.NewLine;
+                    }
+                    catch
+                    {
+                        textBox4.Text += "y(" + i.ToString() + ")=error" + Environment.NewLine;
+                    }
+            }
+            catch (FormatException)
+            {
+                textBox4.Text = "Неверный формат ввода данных";
+            }
+            catch
+            {
+                textBox4.Text = "Неизвестная ошибка";
+            }
+        }
+        static double F(double x)
+        {
+            try
+            {
+                //если х не попадает в область определения, то генерируется исключение 
+                if (x == -1) throw new Exception();
+                else return (x + 4 / x * x - 2) + Math.Sqrt(x * x * x - 1);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Консоль5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Консоль5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("a=");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("b=");
+                double b = double.Parse(Console.ReadLine());
+                Console.Write("h=");
+                double h = double.Parse(Console.ReadLine());
+                for (double i = a; i <= b; i += h)
+                    try
+                    {
+                        Console.WriteLine("y({0})={1:f4}", i, F(i));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("y({0})=error", i);
+                    }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный формат ввода данных");
+            }
+            catch
+            {
+                Console.WriteLine("Неизвестная ошибка");
+            }
+            Console.ReadKey();
+        }
+        static double F(double x)
+        {
+            try
+            {
+                //если х не попадает в область определения, то генерируется исключение 
+                if (x == -1) throw new Exception();
+                else return (x + 4 / x * x - 2) + Math.Sqrt(x * x * x - 1);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
